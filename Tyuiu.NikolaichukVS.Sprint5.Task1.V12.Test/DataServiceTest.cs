5b5d1bc2c87333c1cba5029d853b6a094f3a4af3
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-11-19 02:07:33 +0500

    Task1

---
+++ Tyuiu.NikolaichukVS.Sprint5.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.NikolaichukVS.Sprint5.Task1.V12.Lib;
+
+namespace Tyuiu.NikolaichukVS.Sprint5.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\ТИУ\source\repos\Tyuiu.NikolaichukVS.Sprint5\Tyuiu.NikolaichukVS.Sprint5.Task1.V12\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}

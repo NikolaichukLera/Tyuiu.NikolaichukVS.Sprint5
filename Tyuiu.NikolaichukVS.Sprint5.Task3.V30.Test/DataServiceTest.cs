c87a67053b1a83b686c55f2191d000970c85c0d6
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-11-19 21:34:04 +0500

    Task3

---
+++ Tyuiu.NikolaichukVS.Sprint5.Task3.V30.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NikolaichukVS.Sprint5.Task3.V30.Lib;
+
+using System.IO;
+
+namespace Tyuiu.NikolaichukVS.Sprint5.Task3.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\ТИУ\source\repos\Tyuiu.NikolaichukVS.Sprint5\Tyuiu.NikolaichukVS.Sprint5.Task3.V30\bin\Debug\OutPutFileTask3.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.NikolaichukVS.Sprint5.Task3.V30/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NikolaichukVS.Sprint5.Task3.V30.Lib;
+
+namespace Tyuiu.NikolaichukVS.Sprint5.Task3.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Николайчук В. С.| РПСБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл.                    *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнила: Николайчук Валерия Сергеевна | РПСБ-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение                                                          *");
+            Console.WriteLine("*         x^3 - 1                                                         *");
+            Console.WriteLine("* y =  ------------                                                       *");
+            Console.WriteLine("*          4x^2                                                           *");
+            Console.WriteLine("* вычислить его значение при x = 3, результат сохранить в бинарный файл   *");
+            Console.WriteLine("* OutPutFileTask3.bin и вывести на консоль. Округлить до трёх знаков      *");
+            Console.WriteLine("* после запятой.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+          
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    
+    }
+}

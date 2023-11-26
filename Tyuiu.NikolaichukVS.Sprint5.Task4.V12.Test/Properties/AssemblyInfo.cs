1414f496225e3bec693743b5ed5c0262749349a5
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-11-26 16:36:59 +0500

    Task4

---
+++ Tyuiu.NikolaichukVS.Sprint5.Task4.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NikolaichukVS.Sprint5.Task4.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NikolaichukVS.Sprint5.Task4.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2319e423-5b56-41e1-99a6-1931e1033f23")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
